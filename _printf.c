188d6f4024b725caf7db7c4375379d5ee646e58e
Alexthuita <colealex400@gmail.com>
2022-04-18 13:29:11 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -93,3 +93,19 @@ int check_percent(int *flag, char aux)
 	}
 	return (cont);
 }
+
+/**
+ * call_function_manager - call function manager
+ *@aux: character parameter
+ *@arg: va_list arg
+ *Description: This function call function manager
+ *Return: num of characteres printed
+ */
+
+int call_function_manager(char aux, va_list arg)
+{
+	int cont = 0;
+
+	cont = function_manager(aux, arg);
+	return (cont);
+}
